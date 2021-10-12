865d502395fb6ef2be1be473856303a8733ae70f
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-12 05:44:15 +0300

    changes

--- quotes-creation/src/app/date-pipe.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DatePipe.Pipe } from './date-pipe.pipe';
-
-describe('DatePipe.Pipe', () => {
-  it('should create an instance', () => {
-    expect(new DatePipe.Pipe()).toBeTruthy();
-  });
-});
